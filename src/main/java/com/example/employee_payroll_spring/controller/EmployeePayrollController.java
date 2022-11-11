99febd3ad6ea982ff63cc033105e8b9470dece82
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-11-11 10:22:57 +0530

    added:rest request from employee payroll

--- src/main/java/com/example/employee_payroll_spring/controller/EmployeePayrollController.java
+++ src/main/java/com/example/employee_payroll_spring/controller/EmployeePayrollController.java
@@ -1,4 +1,16 @@
 package com.example.employee_payroll_spring.controller;
 
+import com.example.employee_payroll_spring.service.EmployeePayrollService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeePayrollController {
+    @Autowired
+    EmployeePayrollService employeePayrollService;
+    @PostMapping("/post")
+    public String post(){
+        return EmployeePayrollService.printMessages();
+    }
 }

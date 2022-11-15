efbf1f8f30adfdbac67c16de6a2c71eb16587d58
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-11-15 21:42:40 +0530

    added:Employee payroll using different staging loggin lavels

--- src/main/java/com/example/employee_payroll_spring/EmployeePayrollSpringApplication.java
+++ src/main/java/com/example/employee_payroll_spring/EmployeePayrollSpringApplication.java
@@ -3,14 +3,18 @@ package com.example.employee_payroll_spring;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayrollSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollSpringApplication.class, args);
-        log.info("Employee pay roll app started");
+      // SpringApplication.run(EmployeePayrollSpringApplication.class, args);
+        ApplicationContext context = SpringApplication.run(EmployeePayrollSpringApplication.class, args);
+        log.info("Employee payroll app started in {} Environment " + context.getEnvironment().getProperty("environment"));
+    }
     }
 
-}
+
